7a49ca95ceb51cc0985443093a7db6f021afc805
Brayan Tabares <brayantabares10a2014@gmail.com>
2020-02-09 20:32:12 -0500

    configured the spotify account

--- app/src/main/java/app/example/spotifytest/MainActivity.kt
+++ app/src/main/java/app/example/spotifytest/MainActivity.kt
@@ -2,11 +2,81 @@ package app.example.spotifytest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.spotify.android.appremote.api.ConnectionParams
+import com.spotify.android.appremote.api.Connector.ConnectionListener
+import com.spotify.android.appremote.api.SpotifyAppRemote
 
 class MainActivity : AppCompatActivity() {
+
+    //<editor-fold desc="Vars">
+
+    private val CLIENT_ID = "03f40f0ce41748fb97637f2ddd707b36"
+    private val REDIRECT_URI = "http://app.example.spotifytest/callback/"
+//    private val REDIRECT_URI = "https://www.google.com/"
+    private var mSpotifyAppRemote: SpotifyAppRemote? = null
+
+    //</editor-fold>
+
+    //<editor-fold desc="Life Cycle">
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onStart() {
+        super.onStart()
+        val connectionParams = ConnectionParams.Builder(CLIENT_ID)
+            .setRedirectUri(REDIRECT_URI)
+            .showAuthView(true)
+            .build()
+
+        SpotifyAppRemote.connect(this, connectionParams,
+            object : ConnectionListener {
+
+                override fun onConnected(spotifyAppRemote: SpotifyAppRemote) {
+                    mSpotifyAppRemote = spotifyAppRemote
+                    Log.d("MainActivity", "Connected! Yay!")
+
+                    // Now you can start interacting with App Remote
+                    connected()
+                }
+
+                override fun onFailure(throwable: Throwable) {
+                    Log.e("MainActivity", throwable.message, throwable)
+                    // Something went wrong when attempting to connect! Handle errors here
+
+                    Toast.makeText(applicationContext, "Be sure to have and log-in in Spotify", Toast.LENGTH_SHORT).show()
+                }
+            })
+    }
+
+    override fun onStop() {
+        super.onStop()
+        SpotifyAppRemote.disconnect(mSpotifyAppRemote)
+    }
+
+    //</editor-fold>
+
+    //<editor-fold desc="Connected">
+
+    private fun connected() {
+        // Play a playlist
+        mSpotifyAppRemote?.playerApi?.play("spotify:playlist:37i9dQZF1DX2sUQwD7tbmL")
+
+        // Subscribe to PlayerState
+        mSpotifyAppRemote?.playerApi?.
+            subscribeToPlayerState()?.
+            setEventCallback { playerState ->
+                val track = playerState.track
+                if (track != null) {
+                    Log.d("MainActivity", track.name + " by " + track.artist.name)
+                }
+            }
+    }
+
+    //</editor-fold>
+
 }
