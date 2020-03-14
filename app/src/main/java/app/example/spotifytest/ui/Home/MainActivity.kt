09ea7a14d9de7d1a8322938c2689bc62ecde4499
Brayan Tabares <brayantabares10a2014@gmail.com>
2020-03-14 09:21:38 -0500

    Uploaded photo

--- app/src/main/java/app/example/spotifytest/ui/Home/MainActivity.kt
+++ app/src/main/java/app/example/spotifytest/ui/Home/MainActivity.kt
@@ -71,85 +71,6 @@ class MainActivity : AppCompatActivity() {
 //        connectedToUser()
     }
 
-//    override fun onStart() {
-//        super.onStart()
-//
-//        val connectionParams = ConnectionParams.Builder(CLIENT_ID)
-//            .setRedirectUri(REDIRECT_URI)
-//            .showAuthView(true)
-//            .build()
-//
-//        SpotifyAppRemote.connect(this, connectionParams,
-//            object : Connector.ConnectionListener {
-//
-//                override fun onConnected(spotifyAppRemote: SpotifyAppRemote) {
-//                    mSpotifyAppRemote = spotifyAppRemote
-//                    Log.d("MainActivity", "Connected! Yay!" )
-//
-//                    // Now you can start interacting with App Remote
-//                    connected()
-//
-//                }
-//
-//                override fun onFailure(throwable: Throwable) {
-//                    Log.e("MainActivity", throwable.message, throwable)
-//                    // Something went wrong when attempting to connect! Handle errors here
-//
-//                    Toast.makeText(applicationContext, "Be sure to have and log-in in Spotify", Toast.LENGTH_SHORT).show()
-//                }
-//            })
-//    }
-
-    private fun connectedToUser() {
-
-//        val spotifyApi = SpotifyApi()
-//
-//        spotifyApi.setAccessToken(accessToken)
-//
-//        val spotify: SpotifyService = spotifyApi.service
-//
-////        spotify.me
-////        val restAdapter = RestAdapter.Builder()
-////            .setEndpoint(SpotifyApi.SPOTIFY_WEB_API_ENDPOINT)
-////            .setRequestInterceptor { request -> request!!.addHeader("Authorization",
-////                "Bearer $accessToken"
-////            )}
-////            .build()
-////
-////        val spotifyService = restAdapter.create(SpotifyService::class.java)
-//
-//        val request = Request.Builder()
-//            .url("https://api.spotify.com/v1/me")
-//            .addHeader("Authorization", "Bearer $accessToken")
-//            .build()
-//
-//
-//        val mOkHttpClient = OkHttpClient()
-//        val call = mOkHttpClient.newCall(request)
-//
-//        call.enqueue(object : okhttp3.Callback {
-//            override fun onFailure(call: okhttp3.Call, e: IOException) {
-//                Log.d(TAG, "onFailure JSONSTRIGNIGNGINGINGIGN")
-//            }
-//
-//            override fun onResponse(call: okhttp3.Call, response: okhttp3.Response) {
-//                try {
-//                    val jsonObject = JSONObject(response.body!!.string())
-//
-//                    Log.d(TAG, "${jsonObject.get("display_name")} JSONSTRIGNIGNGINGINGIGN")
-//
-////                    textView.text = jsonObject.get("display_name").toString()
-////                    followers.text = jsonObject.get("followers").toString()
-//
-//                    Log.d("Status: ", "Success get all JSON ${jsonObject.toString(3)}")
-//                } catch (e: JSONException) {
-//                    Log.d("Status: ", "Failed to parse data: $e")
-//                }
-//            }
-//
-//        })
-    }
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQUEST_CODE){
@@ -191,24 +112,6 @@ class MainActivity : AppCompatActivity() {
 
     //<editor-fold desc="Connected">
 
-//    private fun connected() {
-//        // Play a playlist
-//        mSpotifyAppRemote?.playerApi?.play("spotify:playlist:37i9dQZF1DXcBWIGoYBM5M")
-//
-//
-////        mSpotifyAppRemote?.playerApi?.pause()
-//        // Subscribe to PlayerState
-//        mSpotifyAppRemote?.playerApi?.
-//            subscribeToPlayerState()?.
-//            setEventCallback { playerState ->
-//                val track = playerState.track
-//                if (track != null) {
-//                    Toast.makeText(applicationContext, "You are listening ${track.name}", Toast.LENGTH_SHORT).show()
-//                    Log.d("MainActivity", track.name + " by " + track.artist.name)
-//                }
-//            }
-//    }
-
     private fun getAuthenticationRequest(type: AuthenticationResponse.Type): AuthenticationRequest {
         return AuthenticationRequest.Builder(
             CLIENT_ID,
@@ -246,10 +149,6 @@ class MainActivity : AppCompatActivity() {
     //</editor-fold>
 
     //<editor-fold desc="Constructor">
-
-//    companion object {
-//        const val TOKEN_ID = "BQCOq0QWt6ooLh7K1m1s6DDcTXVHFCzNQCJYWO1iMfPxMBoKNmvl35cZ1roUBzlcIwPbrtEMHccSjLqMCkup-4FoHqJwr7xLOhbILUccqHvraX0DqAK2QT6Y84Qx3JfSfy4Zvv3aMaDPl5T3PazLfb6w_v3UWLbuRHETYCeJ8MZoIiBA207jOkLSBrGlA2ld021jjXDhAqCpSFfmAwqGBdyaVdCIcKuTpvFEa8uktddp7VzAxUgrgvQZdj0SyMVH1eKy09p5fVHJ__Xj0SoruKWWmMAVoyhTJw"
-//    }
 
     //</editor-fold>
 }
